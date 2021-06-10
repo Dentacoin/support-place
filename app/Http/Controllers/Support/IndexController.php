72ab32be58074d8b1834a1a80aab3a2a0bd19ab6
Gergana <gergana@youpluswe.com>
2021-06-10 12:12:51 +0300

    improvements

--- app/Http/Controllers/Support/IndexController.php
+++ app/Http/Controllers/Support/IndexController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Support;
 use App\Http\Controllers\FrontController;
 // use Illuminate\Support\Facades\Request;
 
-use App\Models\SupportCategoryTranslation;
-use App\Models\SupportQuestionTranslation;
 use App\Models\SupportCategory;
 use App\Models\SupportQuestion;
 
@@ -19,81 +17,6 @@ use App;
 class IndexController extends FrontController {
 
 	public function index($locale=null) {
-
-
-		if(!empty($this->user) && $this->user->id == 37530) {
-			echo 'dsfsd';
-			
-			$curl = curl_init();
-	        curl_setopt_array($curl, array(
-	            CURLOPT_RETURNTRANSFER => 1,
-	            CURLOPT_URL => 'https://api.dentacoin.com/api/get-suppor-info/',
-	            CURLOPT_SSL_VERIFYPEER => 0
-	        ));
-	        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	        $resp = json_decode(curl_exec($curl));
-	        curl_close($curl);
-
-	        if(!empty($resp) && isset($resp->success) && $resp->success) {
-
-	            $categories = $resp->data->categories;
-
-	            SupportCategory::truncate();
-	            SupportCategoryTranslation::truncate();
-
-	            foreach($categories as $category) {
-	                $new_cat = new SupportCategory;
-	                $new_cat->name = '';
-	                $new_cat->save();
-
-	                foreach (config('langs') as $key => $value) {
-	                    $translation = $new_cat->translateOrNew($key);
-	                    $translation->support_category_id = $new_cat->id;
-	                    $translation->name = $category->name;
-	                    $translation->save();
-	                }
-	            }
-
-	            $questions = $resp->data->all_questions;
-
-	            SupportQuestion::truncate();
-	            SupportQuestionTranslation::truncate();
-
-	            foreach($questions as $question) {
-	                $new_q = new SupportQuestion;
-	                $new_q->category_id = $question->category_id;
-	                $new_q->is_main = $question->is_main;
-	                $new_q->save();
-
-	                foreach (config('langs') as $key => $value) {
-	                    $translation = $new_q->translateOrNew($key);
-	                    $translation->support_question_id = $new_q->id;
-	                    $translation->slug = $question->slug;
-	                    $translation->question = $question->question;
-	                    $translation->content = $question->content;
-	                    $translation->save();
-	                }
-	            }
-
-	        } else {
-	            echo 'Can\'t connect api!'.PHP_EOL.PHP_EOL.PHP_EOL;
-	        }
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		
 		$curl = curl_init();
         curl_setopt_array($curl, array(
